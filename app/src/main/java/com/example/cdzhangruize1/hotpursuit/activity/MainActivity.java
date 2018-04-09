bc4f3ff69411ed0e55dc8f765f57476fd9387e6f
cdzhangruize1 <cdzhangruize1@jd.com>
2018-04-09 11:26:06 +0800

    使用RxJava，改造加载方法

--- app/src/main/java/com/example/cdzhangruize1/hotpursuit/activity/MainActivity.java
+++ app/src/main/java/com/example/cdzhangruize1/hotpursuit/activity/MainActivity.java
@@ -3,17 +3,14 @@ package com.example.cdzhangruize1.hotpursuit.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.cdzhangruize1.hotpursuit.R;
 import com.example.cdzhangruize1.hotpursuit.engine.ListEngine;
 import com.example.cdzhangruize1.hotpursuit.engine.WebEngine;
 import com.example.cdzhangruize1.hotpursuit.model.BaseScraperModel;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     WebEngine mWebEngine = new WebEngine();
@@ -28,24 +25,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        mWebEngine.setmLoadCallback(new WebEngine.LoadCallback() {
+        mWebEngine.load(createTestModel(), mListEngine, this, new WebEngine.LoadCallback2() {
             @Override
-            public void onSucceed(ArrayList<HashMap<String, String>> data) {
-                final RecyclerView view = mListEngine.load(data, MainActivity.this);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((ViewGroup) findViewById(R.id.container)).addView(view,
-                                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                    }
-                });
+            public void onSucceed(RecyclerView view) {
+                ((ViewGroup) findViewById(R.id.container)).addView(view,
+                        new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             }
 
             @Override
             public void onFailed() {
+                Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
             }
         });
-        mWebEngine.load(createTestModel());
     }
 
     private BaseScraperModel createTestModel() {
--- app/src/main/java/com/example/cdzhangruize1/hotpursuit/engine/WebEngine.java
+++ app/src/main/java/com/example/cdzhangruize1/hotpursuit/engine/WebEngine.java
@@ -1,5 +1,9 @@
 package com.example.cdzhangruize1.hotpursuit.engine;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
 import com.example.cdzhangruize1.hotpursuit.model.BaseScraperModel;
 
 import org.jsoup.Jsoup;
@@ -11,44 +15,78 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
 public class WebEngine {
-    LoadCallback mLoadCallback;
-    BaseScraperModel mScraperModel;
+    private ObservableEmitter<LoadTask> mEmitter;
 
+    @SuppressLint("CheckResult")
     public WebEngine() {
-    }
-
-    public void load(BaseScraperModel model) {//todo 我们或许应该在每一次Load都开启一次新的线程任务。可以支持并发load
-        mScraperModel = model;
-        mWorkThread.start();
-    }
-
-    public void setmLoadCallback(LoadCallback mLoadCallback) {
-        this.mLoadCallback = mLoadCallback;
-    }
-
-    public interface LoadCallback {
-        void onSucceed(ArrayList<HashMap<String, String>> data);
-
-        void onFailed();
-    }
-
-    private Thread mWorkThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            String url = mScraperModel.links.get(0);
-            try {
-                Document d = Jsoup.connect(url).get();
-                processDocument(d);
-            } catch (IOException e) {
-                e.printStackTrace();
+        Observable.create(new ObservableOnSubscribe<LoadTask>() {
+            @Override
+            public void subscribe(ObservableEmitter<LoadTask> emitter) throws Exception {
+                mEmitter = emitter;
             }
+        }).observeOn(Schedulers.computation()).flatMap(new Function<LoadTask, ObservableSource<LoadTask>>() {
+            @Override
+            public ObservableSource<LoadTask> apply(LoadTask loadTask) throws Exception {
+                String url = loadTask.model.links.get(0);
+                try {
+                    Document d = Jsoup.connect(url).get();
+                    loadTask.processDocument(d);
+                    loadTask.generateRecyclerView();
+                } catch (IOException e) {
+                    loadTask.callback.onFailed();
+                }
+                return Observable.just(loadTask);
+            }
+        }).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<LoadTask>() {
+            @Override
+            public void accept(LoadTask loadTask) throws Exception {
+                loadTask.callback.onSucceed(loadTask.view);
+            }
+        });
+    }
+
+    /**
+     * 创建一个任务，获取数据后生成列表
+     *
+     * @param model      网络模型
+     * @param listEngine 列表生成引擎
+     * @param context    用来生成列表
+     * @param callback   回调
+     */
+    public void load(BaseScraperModel model, ListEngine listEngine, Context context, LoadCallback2 callback) {
+        mEmitter.onNext(new LoadTask(model, callback, listEngine, context));
+    }
+
+    class LoadTask {
+        BaseScraperModel model;
+        LoadCallback2 callback;
+        ListEngine listEngine;
+        Context context;
+
+        ArrayList<HashMap<String, String>> data = new ArrayList<>();
+        RecyclerView view;
+
+        LoadTask(BaseScraperModel model, LoadCallback2 callback, ListEngine listEngine, Context context) {
+            this.model = model;
+            this.callback = callback;
+            this.listEngine = listEngine;
+            this.context = context;
         }
 
-        private void processDocument(Document d) {
-            ArrayList<HashMap<String, String>> dataResult = new ArrayList<>();
-            for (String path : mScraperModel.xpathMaps.keySet()) {
-                String mapTo = mScraperModel.xpathMaps.get(path);
+        public void processDocument(Document d) {
+            data.clear();
+            for (String path : model.xpathMaps.keySet()) {
+                String mapTo = model.xpathMaps.get(path);
                 boolean needBreak = false;
                 int count = 1;
                 while (!needBreak) {
@@ -56,19 +94,26 @@ public class WebEngine {
                     Element e = d.selectFirst(sel);
                     if (e != null && e.childNodeSize() > 0 && e.childNode(0) instanceof TextNode) {
                         String value = ((TextNode) e.childNode(0)).text().trim();
-                        if (dataResult.size() < count) {
-                            dataResult.add(new HashMap<String, String>());
+                        if (data.size() < count) {
+                            data.add(new HashMap<String, String>());
                         }
-                        dataResult.get(count - 1).put(mapTo, value);
+                        data.get(count - 1).put(mapTo, value);
                     } else {
                         needBreak = true;
                     }
                     count++;
                 }
             }
-            if (mLoadCallback != null) {
-                mLoadCallback.onSucceed(dataResult);
-            }
         }
-    });
+
+        public void generateRecyclerView() {
+            view = listEngine.load(data, context);
+        }
+    }
+
+    public interface LoadCallback2 {
+        void onSucceed(RecyclerView view);
+
+        void onFailed();
+    }
 }
